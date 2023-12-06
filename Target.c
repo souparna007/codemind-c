64b2dc1010bdf986d869b137190da0e140dab61c
souparna007 <151903432+souparna007@users.noreply.github.com>
2023-12-06 14:03:23 +0530

    #include<stdio.h> int main() {     int a,b,c,d;     scanf("%d %d %d %d",&a,&b,&c,&d);     //int x=a+b+c+d;     if(a>=10 && b>=10 && c>=10 && d>=10)     {         printf("YES");     }     else     {         printf("NO");     } }

---
+++ Target.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,d;
+    scanf("%d %d %d %d",&a,&b,&c,&d);
+    //int x=a+b+c+d;
+    if(a>=10 && b>=10 && c>=10 && d>=10)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+}
