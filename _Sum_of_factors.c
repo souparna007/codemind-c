2af68bb9408c4243555b55980870ccc902f71855
souparna007 <151903432+souparna007@users.noreply.github.com>
2023-12-07 10:51:25 +0530

    #include<stdio.h> int main() {     int n,sum=0;     scanf("%d",&n);     for(int i=1;i<n;i++)     {         if(n%i==0)         {             sum=sum+i;         }     }      printf("%d",sum); }

---
+++ _Sum_of_factors.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int n,sum=0;
+    scanf("%d",&n);
+    for(int i=1;i<n;i++)
+    {
+        if(n%i==0)
+        {
+            sum=sum+i;
+        }
+    }
+     printf("%d",sum);
+}
