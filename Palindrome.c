d3442d0ad98095bc2746b24e33c80d612f3d05ce
souparna007 <151903432+souparna007@users.noreply.github.com>
2023-12-13 20:49:55 +0530

    #include<stdio.h> int main() {     int n,sum=0;     scanf("%d",&n);     int c=n;     while(n>0)     {         int r=n%10;         sum=r+(sum*10);         n=n/10;     }     if(c==sum)     {         printf("Palindrome");     }     else     {         printf("Not Palindrome");     } }

---
+++ Palindrome.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int n,sum=0;
+    scanf("%d",&n);
+    int c=n;
+    while(n>0)
+    {
+        int r=n%10;
+        sum=r+(sum*10);
+        n=n/10;
+    }
+    if(c==sum)
+    {
+        printf("Palindrome");
+    }
+    else
+    {
+        printf("Not Palindrome");
+    }
+}
