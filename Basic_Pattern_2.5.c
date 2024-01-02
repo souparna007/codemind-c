7f24790d3727d803d29c3dfa3de5f712f1f0608e
souparna007 <151903432+souparna007@users.noreply.github.com>
2024-01-02 21:50:20 +0530

    #include<stdio.h> int main() { 	int i,j,n; 	scanf("%d",&n); 	for(i=n;i>=1;i--) 	{ 	    for(j=i;j<=n;j++) 	    { 	        printf("%c ",i+64); 	    } 	    printf(" "); 	}
    }

---
+++ Basic_Pattern_2.5.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int i,j,n;
+	scanf("%d",&n);
+	for(i=n;i>=1;i--)
+	{
+	    for(j=i;j<=n;j++)
+	    {
+	        printf("%c ",i+64);
+	    }
+	    printf("
+");
+	}
+
+}
