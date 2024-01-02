15692101027d42986aa263f0b7e7da246c928656
souparna007 <151903432+souparna007@users.noreply.github.com>
2024-01-02 22:52:11 +0530

    #include<stdio.h> int main() {     int a,count=0;     scanf("%d",&a);     for(int i=1;i<=a;i++)     {         if(a%i==0)         {             count++;         }     }     if(count==2)     {         printf("Prime");     }     else     {         printf("Not Prime");     } }

---
+++ Prime.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int a,count=0;
+    scanf("%d",&a);
+    for(int i=1;i<=a;i++)
+    {
+        if(a%i==0)
+        {
+            count++;
+        }
+    }
+    if(count==2)
+    {
+        printf("Prime");
+    }
+    else
+    {
+        printf("Not Prime");
+    }
+}
